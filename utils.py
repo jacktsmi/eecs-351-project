9030399471d063478a357570025e22d89c6ea817
jacktsmi <jacktsmi@umich.edu>
2021-03-17 15:48:06 -0400

    Added directory parameter into rename_data function.

--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
 import pathlib
 
-def rename_data():
+def rename_data(dir_path):
+    """
+    Renames all files in dir_path to "trainX" where X is a number.
+
+    Inputs:
+        dir_path : Path to the directory which has the files we wish to rename
+        (For our purposes this will likely be the folder with either the train
+        data or test data)
+    """
     index = 1
-    for path in pathlib.Path("train").iterdir():
+    for path in pathlib.Path(dit_path).iterdir():
         if path.is_file():
             directory = path.parent
             old_extension = path.suffix
