013a6df80ab09b6a3388140850a0835609109490
jacktsmi <jacktsmi@umich.edu>
2021-03-18 12:33:05 -0400

    Added function outlines to the featurizer.

--- featurizer.py
+++ featurizer.py
@@ -1,4 +1,33 @@
 import numpy as np
+import wave
+import sys
+
+def calc_spectral_centroid(song):
+    """
+    Will calculate spectral centroid value of a song. Should be called
+    in featurize().
+
+    Inputs:
+        song : length N vector representing audio signal
+    """
+
+def calc_spectral_bandwidth(song):
+    """
+    Will calculate spectral bandwidth of a song. Should be called
+    in featurize().
+    """
+
+def calc_mfcc(song):
+    """
+    Will calculate MFCC's of a song. Should be called
+    in featurize().
+    """
+
+def calc_chroma(song):
+    """
+    Will calculate chroma of a song. Should be called
+    in featurize().
+    """
 
 def featurize(song):
     """
