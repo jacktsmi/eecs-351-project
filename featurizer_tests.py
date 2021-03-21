7ba248d5913a203460f84f1a1cc6ecec6c0ce586
jacktsmi <jacktsmi@umich.edu>
2021-03-21 16:30:35 -0400

    Uses wavfile to read in songs instead of librosa. Is much faster.

--- featurizer_tests.py
+++ featurizer_tests.py
@@ -7,9 +7,16 @@ import librosa
 
 # Plots to see if spectral centroid appears to be the weighted average frequency.
 # To run, select and right-click and select "Run Selection/Line in Interactive Window"
-data, fs = librosa.load('./train/train700.wav')
-data = np.array(data)
-data = ut.middle_n(data, 2000000)
-sc = ft.calc_spectral_centroid(data, fs)
-plt.plot(sc)
+fs, data1 = wavfile.read('./train/train500.wav') # Calm Song
+data1 = data1[:, 0] # Take only one channel
+data1 = np.array(data1)
+data1 = ut.middle_n(data1, 2000000)
+sc1 = ft.calc_spectral_centroid(data1, fs)
+fs, data2 = wavfile.read('./train/train700.wav') # Hype Song
+data2 = data2[:, 0] # Take only one channel
+data2 = np.array(data2)
+data2 = ut.middle_n(data2, 2000000)
+sc2 = ft.calc_spectral_centroid(data2, fs)
+plt.plot(sc1, 'b')
+plt.plot(sc2, 'r')
 plt.show()
