f8705ffac2c2bfea00b7670010af90a5885103e2
jacktsmi <jacktsmi@umich.edu>
2021-03-23 16:11:37 -0400

    Wrote calc_mfcc function and updated tests file to give sample plots.

--- featurizer.py
+++ featurizer.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.io import wavfile
 import sys
+from scipy.fftpack import dct
 
 def calc_spectral_centroid(song, fs, frame_size=1000):
     """
@@ -36,11 +37,61 @@ def calc_spectral_bandwidth(song):
     in featurize().
     """
 
-def calc_mfcc(song):
+def calc_mfcc(song, fs, frame_size=1000):
     """
     Will calculate MFCC's of a song. Should be called
     in featurize().
     """
+    mfcc = np.zeros((int(song.shape[0] / frame_size)))
+    pre_emphasis = 0.97
+    emph_signal = np.append(song[0], song[1:] - pre_emphasis * song[:-1])
+    frame_stride = 500 # Resulting in 500 sample overlap
+
+    song_length = len(emph_signal)
+    frame_length = int(round(frame_size))
+    frame_step = int(round(frame_stride))
+    num_frames = int(np.ceil(float(np.abs(song_length - frame_length)) / frame_step))
+
+    pad_song_length = num_frames * frame_step + frame_length
+    z = np.zeros((pad_song_length - song_length))
+    pad_song = np.append(emph_signal, z)
+
+    indices = np.tile(np.arange(0, frame_length), (num_frames, 1)) + np.tile(np.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
+    frames = pad_song[indices.astype(np.int32, copy=False)]
+
+    frames *= np.hamming(frame_length)
+
+    NFFT = 512
+    mag_frames = np.absolute(np.fft.rfft(frames, NFFT))
+    pow_frames = ((1.0 * NFFT) * ((mag_frames) ** 2))
+
+    nfilt = 40
+
+    low_freq_mel = 0
+    high_freq_mel = (2595 * np.log10(1 + (fs / 2) / 700))
+    mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2)
+    hz_points = (700 * (10**(mel_points / 2595) - 1))
+    bin = np.floor((NFFT + 1) * hz_points / fs)
+
+    fbank = np.zeros((nfilt, int(np.floor(NFFT / 2 + 1))))
+    for m in range(1, nfilt + 1):
+        f_m_minus = int(bin[m-1])
+        f_m = int(bin[m])
+        f_m_plus = int(bin[m + 1])
+
+        for k in range(f_m_minus, f_m):
+            fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+        for k in range(f_m, f_m_plus):
+            fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+    filter_banks = np.dot(pow_frames, fbank.T)
+    filter_banks = np.where(filter_banks == 0, np.finfo(float).eps, filter_banks)
+    filter_banks = 20 * np.log10(filter_banks)
+
+    num_ceps = 12
+    mfcc = dct(filter_banks, type=2, axis=1, norm='ortho')[:, 1 : (num_ceps + 1)]
+    
+    return mfcc
+    
 
 def calc_chroma(song):
     """
--- featurizer_tests.py
+++ featurizer_tests.py
@@ -7,16 +7,26 @@ import librosa
 
 # Plots to see if spectral centroid appears to be the weighted average frequency.
 # To run, select and right-click and select "Run Selection/Line in Interactive Window"
-fs, data1 = wavfile.read('./train/train500.wav') # Calm Song
+fs1, data1 = wavfile.read('./train/train500.wav') # Calm Song
 data1 = data1[:, 0] # Take only one channel
 data1 = np.array(data1)
 data1 = ut.middle_n(data1, 2000000)
-sc1 = ft.calc_spectral_centroid(data1, fs)
-fs, data2 = wavfile.read('./train/train700.wav') # Hype Song
+sc1 = ft.calc_spectral_centroid(data1, fs1)
+fs2, data2 = wavfile.read('./train/train700.wav') # Hype Song
 data2 = data2[:, 0] # Take only one channel
 data2 = np.array(data2)
 data2 = ut.middle_n(data2, 2000000)
-sc2 = ft.calc_spectral_centroid(data2, fs)
-plt.plot(sc1, 'b')
-plt.plot(sc2, 'r')
+sc2 = ft.calc_spectral_centroid(data2, fs2)
+#plt.plot(sc1, 'b')
+#plt.plot(sc2, 'r')
+#plt.show()
+
+# MFCC
+mfcc = ft.calc_mfcc(data1, fs1)
+mfcc = np.array(mfcc)
+plt.pcolormesh(mfcc.T)
+plt.show()
+mfcc2 = ft.calc_mfcc(data2, fs2)
+mfcc2 = np.array(mfcc2)
+plt.pcolormesh(mfcc2.T)
 plt.show()
