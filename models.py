d687bbfe077d15cd4ce94c15f4c66a4cbb3fabed
jacktsmi <jacktsmi@umich.edu>
2021-04-04 13:35:50 -0400

    Inital design of the network we will try to use for classification.

---
+++ models.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(8000, 4000 ,bias=True),
+            nn.ReLU(),
+            nn.Linear(4000, 1000, bias=True),
+            nn.ReLU(),
+            nn.Linear(1000, 200, bias=True),
+            nn.ReLU(),
+            nn.Linear(200, 4, bias=True)
+        )
+    
+    def forward(self, x):
+        out = self.main(x)
+        return out
