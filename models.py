774552aedd18b6bf138684f74d51f7167b2f9565
jacktsmi <jacktsmi@umich.edu>
2021-04-05 17:00:15 -0400

    Fixed description of output in calc_mfcc and wrote initial dataloader file and class. Unsure if it will work. Also changed NN to Multilayer Perceptron model.

--- models.py
+++ models.py
@@ -5,14 +5,14 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        input_dim = 8000 # Unsure
+        hidden_dim = 1000
+        num_moods = 4
         self.main = nn.Sequential(
-            nn.Linear(8000, 4000 ,bias=True),# Unsure on 8000 dimension because I think mfcc outputs 2d array
+            nn.Linear(input_dim, hidden_dim ,bias=True),
             nn.ReLU(),
-            nn.Linear(4000, 1000, bias=True),
-            nn.ReLU(),
-            nn.Linear(1000, 200, bias=True),
-            nn.ReLU(),
-            nn.Linear(200, 4, bias=True)
+            nn.Linear(hidden_dim, num_moods, bias=True),
+            nn.Softmax()
         )
     
     def forward(self, x):
