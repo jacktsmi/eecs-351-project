9aca4440c92352808429af6229e166a3567d0f58
saakethm <saakethm@umich.edu>
2021-04-09 19:13:40 -0400

    Chroma finished - need to test

--- featurizer.py
+++ featurizer.py
@@ -200,8 +200,7 @@ def calc_chroma(song, fs, frame_size=1000):
                     break
 
     mag_chroma = np.zeros((num_frames, 12))
-    # p = 0 correspond
-    # s to C, p = 127 corresponds to G
+
     for p in range(0, 127):
         mag_chroma[(p - 60) % 12] = mag_pitch[p]
 
