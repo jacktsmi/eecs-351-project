81bac6b262721fb4bee8b284975ff65992f94693
saakethm <saakethm@umich.edu>
2021-04-13 18:32:06 -0400

    Nearest Neightbors Updated

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-train_sc = np.zeros((250, 685))
-train_sb = np.zeros((250, 685))
-train_mfcc = np.zeros((5976, 685))
-train_chroma = np.zeros((25*12, 685))
+train_sc = np.zeros((250, 148*4))
+train_sb = np.zeros((250, 148*4))
+train_mfcc = np.zeros((5976, 148*4))
+train_chroma = np.zeros((25*12, 148*4))
 
 test_sc = np.zeros((250, 44))
 test_sb = np.zeros((250, 44))
@@ -59,7 +59,7 @@ for ind in range(1, 730):
         test_i += 1
         label = 3 # Hype
         test_targets.append(label)
-    elif 12 <= ind <= 220:
+    elif 12 <= ind <= 12+147:
         train_sc[:,train_i] = sc
         train_sb[:,train_i] = sb
         train_mfcc[:,train_i] = mfcc
@@ -67,7 +67,7 @@ for ind in range(1, 730):
         train_i += 1
         label = 0 # Happy
         train_targets.append(label)
-    elif 232 <= ind <= 379:
+    elif 232 <= ind <= 232+147:
         train_sc[:,train_i] = sc
         train_sb[:,train_i] = sb
         train_mfcc[:,train_i] = mfcc
@@ -75,7 +75,7 @@ for ind in range(1, 730):
         train_i += 1
         label = 1 # Sad
         train_targets.append(label)
-    elif 391 <= ind <= 548:
+    elif 391 <= ind <= 391+147:
         train_sc[:,train_i] = sc
         train_sb[:,train_i] = sb
         train_mfcc[:,train_i] = mfcc
@@ -83,7 +83,7 @@ for ind in range(1, 730):
         train_i += 1
         label = 2 # Calm
         train_targets.append(label)
-    elif 560 <= ind <= 729:
+    elif 560 <= ind <= 560+147:
         train_sc[:,train_i] = sc
         train_sb[:,train_i] = sb
         train_mfcc[:,train_i] = mfcc
@@ -92,15 +92,15 @@ for ind in range(1, 730):
         label = 3 # Hype
         train_targets.append(label)
 
-for k in range(1, 30): 
+for k in range(1, 60):
 
     pred_labels = []
 
     for j in range(test_sc.shape[1]): 
-        sc_diff = train_sc - np.tile(test_sc[:,j], (685,1)).T
-        sb_diff = train_sb - np.tile(test_sb[:,j], (685,1)).T
-        mfcc_diff = train_mfcc - np.tile(test_mfcc[:,j], (685,1)).T
-        chroma_diff = train_chroma - np.tile(test_chroma[:,j], (685,1)).T
+        sc_diff = train_sc - np.tile(test_sc[:,j], (148*4,1)).T
+        sb_diff = train_sb - np.tile(test_sb[:,j], (148*4,1)).T
+        mfcc_diff = train_mfcc - np.tile(test_mfcc[:,j], (148*4,1)).T
+        chroma_diff = train_chroma - np.tile(test_chroma[:,j], (148*4,1)).T
 
         sc_norm = np.linalg.norm(sc_diff,axis = 0)
         sb_norm = np.linalg.norm(sb_diff,axis = 0)
@@ -124,7 +124,7 @@ for k in range(1, 30):
     num_correct = np.sum(np.array(pred_labels) == np.array(test_targets))
     accuracies.append(num_correct*100/len(test_targets))
 
-plt.plot(range(1, 30), accuracies)
+plt.plot(range(1, 60), accuracies)
 plt.xlabel('k')
 plt.ylabel('Accuracies (%)')
 plt.savefig('k_neighbors.png')
